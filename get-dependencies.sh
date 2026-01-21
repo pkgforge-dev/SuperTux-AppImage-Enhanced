58855f91cd610ed0a753e3832865193ba30c71cf
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-21 20:10:24 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm supertux
+pacman -Syu --noconfirm supertux pipewire-audio
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
